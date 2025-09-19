f90efa4e9792a14399ab0e3319dad533f3ac77e3
Fathi_Heelo <s12323885@stu.najah.edu>
2025-09-19 20:57:29 +0300

    Problem Two

---
+++ problems/Problem_02/Problem_2_Print_Full_name.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+void main(){
+Scanner input=new Scanner(System.in);
+    System.out.println("Enter the first name :");
+String FirstName=input.next();
+System.out.println("Enter the last name :");
+String LastName=input.next();
+    System.out.println("Hi, "+FirstName+" "+LastName);
+
+
+}
